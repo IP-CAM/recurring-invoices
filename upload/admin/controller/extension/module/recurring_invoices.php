336b2c1d47160644ee53284b8b386bb62d0b070b
root <root@ip13.maadix.org>
2017-01-11 23:10:19 +0100

    fisrt commit

--- upload/admin/controller/extension/module/recurring_invoices.php
+++ upload/admin/controller/extension/module/recurring_invoices.php
@@ -1,1 +1,89 @@
-/var/www/html/sub5.gadix.net/opencart/upload/admin/controller/extension/module/recurring_invoices.php
+<?php
+class ControllerExtensionModuleRecurringInvoices extends Controller {
+	private $error = array();
+
+	public function index() {
+		$this->load->language('extension/module/recurring_invoices');
+
+		$this->document->setTitle($this->language->get('heading_title'));
+
+		$this->load->model('setting/setting');
+
+		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
+			$this->model_setting_setting->editSetting('recurring_invoices', $this->request->post);
+
+			$this->session->data['success'] = $this->language->get('text_success');
+
+			$this->response->redirect($this->url->link('extension/extension', 'token=' . $this->session->data['token'] . '&type=module', true));
+		}
+
+		$data['heading_title'] = $this->language->get('heading_title');
+
+		$data['text_edit'] = $this->language->get('text_edit');
+		$data['text_enabled'] = $this->language->get('text_enabled');
+		$data['text_disabled'] = $this->language->get('text_disabled');
+
+		$data['entry_status'] = $this->language->get('entry_status');
+
+		$data['button_save'] = $this->language->get('button_save');
+		$data['button_cancel'] = $this->language->get('button_cancel');
+
+		if (isset($this->error['warning'])) {
+			$data['error_warning'] = $this->error['warning'];
+		} else {
+			$data['error_warning'] = '';
+		}
+
+		$data['breadcrumbs'] = array();
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/dashboard', 'token=' . $this->session->data['token'], true)
+		);
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_extension'),
+			'href' => $this->url->link('extension/extension', 'token=' . $this->session->data['token'] . '&type=module', true)
+		);
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('heading_title'),
+			'href' => $this->url->link('extension/module/recurring_invoices', 'token=' . $this->session->data['token'], true)
+		);
+
+		$data['action'] = $this->url->link('extension/module/recurring_invoices', 'token=' . $this->session->data['token'], true);
+
+		$data['cancel'] = $this->url->link('extension/extension', 'token=' . $this->session->data['token'] . '&type=module', true);
+
+		if (isset($this->request->post['recurring_invoices_status'])) {
+			$data['recurring_invoices_status'] = $this->request->post['recurring_invoices_status'];
+		} else {
+			$data['recurring_invoices_status'] = $this->config->get('recurring_invoices_status');
+		}
+
+		$data['header'] = $this->load->controller('common/header');
+		$data['column_left'] = $this->load->controller('common/column_left');
+		$data['footer'] = $this->load->controller('common/footer');
+
+		$this->response->setOutput($this->load->view('extension/module/recurring_invoices', $data));
+	}
+
+	protected function validate() {
+		if (!$this->user->hasPermission('modify', 'extension/module/recurring_invoices')) {
+			$this->error['warning'] = $this->language->get('error_permission');
+		}
+
+		return !$this->error;
+	}
+           public function install() {
+                   $this->load->model("extension/module/recurring_invoices");
+                         $this->model_extension_module_recurring_invoices->createSchema();
+                      }
+         
+           public function uninstall() {
+                   $this->load->model("extension/module/recurring_invoices");
+                         $this->model_extension_module_recurring_invoices->deleteSchema();
+                      }
+
+
+}
